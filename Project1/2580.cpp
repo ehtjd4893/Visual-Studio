4b04da7cad132021205158e53f114d3c192d7915
ehtjd4893 <nsop4893@gmail.com>
2021-03-25 13:21:21 +0900

    2021-03-25 13:21 update

--- Project1/2580.cpp
+++ Project1/2580.cpp
@@ -2,53 +2,76 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
-vector<vector<int>> board(9, vector<int>(9, 0));		// 9x9 벡터 생성
-vector<bool> visited(9, false);			
-vector<int> can = {1,2,3,4,5,6,7,8,9};
-
-bool check_col(int r, int val) {
-	bool result = true;
+vector<vector<int>> board(9, vector<int>(9, 0));	// 9x9 벡터 생성	
+vector<pair<int, int>> points;
+int cnt = 0;
+bool finish = false;
+bool check(int x,int y, int val) {
 	for (int i = 0; i < 9; i++) {
-		if (board[i][r] == val) {
-			result = false;
-			//cout << val << "는 (" << i << "행, "<< r << "열)에 있다\n";
-			return result;
+		if (board[i][x] == val && i != y)
+			return false;
+		if (board[y][i] == val && i != x)
+			return false;
+		
+	}
+	int tmp_x = (x / 3) * 3;
+	int tmp_y = (y / 3) * 3;
+	for (int k = tmp_y; k < tmp_y + 3; k++) {
+		for (int i = tmp_x; i < tmp_x + 3; i++) {
+			if (board[k][i] == val && x != i && y != k)
+				return false;
 		}
-		if (board[i][r] != 0)
-			visited[board[i][r]-1] = true;
 	}
 
-	return result;
-}
-
-bool check_row(int c, int val) {
-	bool result = true;
-	for (int i = 0; i < 9; i++) {
-		if (board[c][i] == val) {
-			result = false;
-			//cout << val << "는 (" << c << "행, " << i << "열)에 있다\n";
-			return result;
-		}
-		if (board[c][i] != 0)
-			visited[board[c][i]-1] = true;
-	}
-	return result;
+	return true;
 }
 
 
-int main() {
-	for (int i = 0; i < 9; i++)
-		for (int k = 0; k < 9; k++)
-			cin >> board[i][k];
-
-	for (int i = 0; i < 9; i++) {
-		check_col(i, 2);
-		check_row(i, 2);
+void sdoqu(int n) {
+	if (n == cnt) {
+		finish = true;
+		return;
 	}
+	else {
+		int x = points[n].first;
+		int y = points[n].second;
+		for (int i = 1; i <= 9; i++) {
+			board[y][x] = i;
+			if (check(x, y, i))
+				sdoqu(n + 1);
+
+			if (finish)
+				return;
+
+		}
+		board[y][x] = 0;
+		return;
+
+	}
+}
+
+void print() {
 	for (int i = 0; i < 9; i++) {
 		for (int k = 0; k < 9; k++)
 			cout << board[i][k] << " ";
 		cout << "\n";
 	}
+}
+
+int main() {
+	for (int i = 0; i < 9; i++)
+		for (int k = 0; k < 9; k++) {
+			cin >> board[i][k];
+			if (board[i][k] == 0) {
+				cnt++;
+				pair<int, int> p;
+				p.first = k; p.second = i;
+				points.push_back(p);
+			}
+		}
+
+	sdoqu(0);
+	
+	print();
 	return 0;
 }
