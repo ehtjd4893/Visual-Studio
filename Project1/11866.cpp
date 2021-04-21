0226c5860e50e5a8aa1b3ac7f0475ba831fa9de8
ehtjd4893 <nsop4893@gmail.com>
2021-04-21 13:37:45 +0900

    2021-04-21 13:37 update

--- Project1/11866.cpp
+++ Project1/11866.cpp
@@ -1,8 +1,39 @@
 #include <iostream>
 #include <queue>
+#include <vector>
 using namespace std;
 
+bool arr[1001];
 int main() {
-
+	
+	vector <int> result;
+	int n, k;
+	cin >> n >> k;
+	
+	int cnt = 0;
+	int b_cnt = 0;
+	int i = 0;
+	while(true){
+		
+		if (!arr[i + 1]) {
+			cnt = (cnt + 1) % k;
+			if (cnt == 0) {
+				//cout << i + 1<< " push\n";
+				result.push_back(i + 1);
+				arr[i + 1] = true;
+				b_cnt++;
+			}
+		}
+		i = (i + 1) % n;
+		if (b_cnt == n)
+			break;
+	}
+	cout << "<";
+	for (int i = 0; i < n; i++) {
+		cout << result[i];
+		if (i != n - 1)
+			cout << ", ";
+	}
+	cout << ">";
 	return 0;
 }
